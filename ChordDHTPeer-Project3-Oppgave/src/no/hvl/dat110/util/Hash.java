2eab9db224b9facd7781013b1a5018fd66704374
LEB <593291@stud.hvl.no>
2022-03-23 14:44:41 +0100

    Task 1: Done Task 2: Done Task 3: Started, but need some correction

--- ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/util/Hash.java
+++ ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/util/Hash.java
@@ -2,13 +2,14 @@ package no.hvl.dat110.util;
 
 /**
  * project 3
- * @author tdoy
+ * @author Kjetil Berg, Kristoffer Vågstøl and Lars Erik Birkeland
  *
  */
 
 import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -16,45 +17,47 @@ public class Hash {
 	
 	private static BigInteger hashint; 
 	
-	public static BigInteger hashOf(String entity) {		
-		
+	public static BigInteger hashOf(String entity) {
 		// Task: Hash a given string using MD5 and return the result as a BigInteger.
-		
 		// we use MD5 with 128 bits digest
-		
+		MessageDigest md;
+		try {
+			md = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			throw new RuntimeException("MD5 does not exist");
+		}
+
 		// compute the hash of the input 'entity'
-		
+		byte[] bytes = entity.getBytes(StandardCharsets.UTF_8);
+		byte[] hashedBytes = md.digest(bytes);
+
 		// convert the hash into hex format
-		
+		String bytesToHex = toHex(hashedBytes); // BUT WHY?
+
 		// convert the hex into BigInteger
-		
+		hashint = new BigInteger(bytesToHex, 16);
+
 		// return the BigInteger
-		
 		return hashint;
 	}
 	
 	public static BigInteger addressSize() {
-		
 		// Task: compute the address size of MD5
-		
 		// get the digest length
-		
 		// compute the number of bits = digest length * 8
-		
+		int length = bitSize();
+
 		// compute the address size = 2 ^ number of bits
-		
 		// return the address size
-		
-		return null;
+		return BigInteger.valueOf(2).pow(length);
 	}
 	
 	public static int bitSize() {
-		
-		int digestlen = 0;
-		
 		// find the digest length
+		// int digestlen = 128;
 		
-		return digestlen*8;
+		return 128*8;
 	}
 	
 	public static String toHex(byte[] digest) {
