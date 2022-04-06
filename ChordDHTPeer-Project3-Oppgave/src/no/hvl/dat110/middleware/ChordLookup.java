c93db842204dd6347879f9eb65891c4f126ef471
LEB <593291@stud.hvl.no>
2022-04-06 12:59:30 +0200

    Added Starter class for starting all processes.

--- ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/middleware/ChordLookup.java
+++ ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/middleware/ChordLookup.java
@@ -30,15 +30,16 @@ public class ChordLookup {
 		// ask this node to find the successor of key
 		// get the successor of the node
 		// NodeInterface successor = node.findSuccessor(key).getSuccessor();
+		// NodeInterface successor = node.findSuccessor(key);
 		NodeInterface successor = node.getSuccessor();
 
 		// get the stub for this successor (Util.getProcessStub())
 		NodeInterface stub = Util.getProcessStub(successor.getNodeName(), successor.getPort());
-		//NodeInterface stub2 = Util.getProcessStub(node.getNodeName(), node.getPort());
+		NodeInterface stub2 = Util.getProcessStub(node.getNodeName(), node.getPort());
 
 		// check that key is a member of the set {nodeid+1,...,succID} i.e. (nodeid+1 <= key <= succID) using the ComputeLogic
 		// boolean member = Util.computeLogic(key, stub.getNodeID().add(BigInteger.valueOf(1)), key.add(BigInteger.valueOf(-1)));
-		boolean member = Util.computeLogic(key, node.getNodeID().add(BigInteger.valueOf(1)), stub.getNodeID());
+		boolean member = Util.computeLogic(key, stub2.getNodeID().add(BigInteger.valueOf(1)), stub.getNodeID());
 
 		// if logic returns true, then return the successor
 		if(member) return successor;
@@ -68,12 +69,12 @@ public class ChordLookup {
 			NodeInterface stub = Util.getProcessStub(finger.getNodeName(), finger.getPort());
 			// check that finger is a member of the set {nodeID+1,...,ID-1}
 			// i.e. (nodeID+1 <= finger <= key-1) using the ComputeLogic
-			boolean member = Util.computeLogic(stub.getNodeID(), node.getNodeID().add(BigInteger.valueOf(1)), key.add(BigInteger.valueOf(-1)));
+			boolean member = Util.computeLogic(stub.getNodeID(), stub2.getNodeID().add(BigInteger.valueOf(1)), key.add(BigInteger.valueOf(-1)));
 			// if logic returns true, then return the finger (means finger is the closest to key)
 			if(member) return finger;
 		}
 		// returns at least one predecessor
-		return node.getPredecessor();
+		return node;
 	}
 	
 	public void copyKeysFromSuccessor(NodeInterface succ) {
